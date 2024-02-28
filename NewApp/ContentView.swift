b63ff9c69c314fc9a3d648e3489d72121f9b739d
Matthew Escobido <matthew.escobido@gmail.com>
2024-02-28 18:11:58 +0800

    Initial Commit

---
+++ NewApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NewApp
+//
+//  Created by firstblock on 2/28/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, class!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NewApp/NewAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewAppApp.swift
+//  NewApp
+//
+//  Created by firstblock on 2/28/24.
+//
+
+import SwiftUI
+
+@main
+struct NewAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
