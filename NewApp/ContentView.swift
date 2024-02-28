0c2edf6d335cb3159054e7e760e6a1e5ae9f2b45
Matthew Escobido <matthew.escobido@gmail.com>
2024-02-28 18:16:37 +0800

    Added tetview edit

--- NewApp/ContentView.swift
+++ NewApp/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, class!")
+            Text("Hello, class! Edit 1")
         }
         .padding()
     }
