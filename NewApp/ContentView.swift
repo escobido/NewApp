cd6fdd6b512efa3c471825270d53f5dce55cad5a
Matthew Escobido <matthew.escobido@gmail.com>
2024-02-28 18:20:19 +0800

    Added text edit 2

--- NewApp/ContentView.swift
+++ NewApp/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, class! Edit 1")
+            Text("Hello, class! Edit 1 + Edit 2")
         }
         .padding()
     }
